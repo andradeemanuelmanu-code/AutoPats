fb18234e8c7926d02eb587c7f6d89ec7a464d178
[dyad] <git@dyad.sh>
2025-11-01 02:33:17 -0300

    [dyad] Replacing Google Maps with OpenStreetMap and Leaflet - wrote 4 file(s), added leaflet, react-leaflet, react-leaflet-markercluster package(s)

--- src/components/mapa/MapComponent.tsx
+++ src/components/mapa/MapComponent.tsx
@@ -1,64 +1,67 @@
-import { GoogleMap, MarkerClustererF, MarkerF, InfoWindowF } from "@react-google-maps/api";
-import { useMemo } from "react";
+import "leaflet/dist/leaflet.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import { useEffect } from "react";
 
-const mapContainerStyle = {
-  width: '100%',
-  height: '100%',
-};
+// Define custom icons using inline SVG, similar to the previous implementation
+const customerSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b82f6" width="32" height="32"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>`;
+const supplierSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#22c55e" width="32" height="32"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>`;
 
-const center = {
-  lat: -14.235004,
-  lng: -51.92528,
-};
+const customerIcon = new L.DivIcon({
+  html: customerSvg,
+  className: '',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+});
 
-const customerIcon = {
-  url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b82f6" width="32" height="32"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>'),
-};
+const supplierIcon = new L.DivIcon({
+  html: supplierSvg,
+  className: '',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+});
 
-const supplierIcon = {
-  url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#22c55e" width="32" height="32"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>'),
+// Helper component to change map view when a location is selected
+const ChangeView = ({ center, zoom }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (center) {
+      map.flyTo(center, zoom, {
+        animate: true,
+        duration: 1,
+      });
+    }
+  }, [center, zoom, map]);
+  return null;
 };
 
 export const MapComponent = ({ locations, selectedLocation, onMarkerClick }) => {
-  const mapOptions = useMemo(() => ({
-    disableDefaultUI: true,
-    clickableIcons: false,
-    scrollwheel: true,
-    zoomControl: true,
-  }), []);
+  const center: L.LatLngExpression = [-14.235004, -51.92528];
+  const selectedPosition = selectedLocation ? [selectedLocation.lat, selectedLocation.lng] as L.LatLngExpression : null;
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      center={center}
-      zoom={4}
-      options={mapOptions}
-    >
-      <MarkerClustererF>
-        {(clusterer) =>
-          locations.map((location) => (
-            <MarkerF
-              key={location.id}
-              position={{ lat: location.lat, lng: location.lng }}
-              clusterer={clusterer}
-              onClick={() => onMarkerClick(location)}
-              icon={location.type === 'customer' ? customerIcon : supplierIcon}
-            />
-          ))
-        }
-      </MarkerClustererF>
-
-      {selectedLocation && (
-        <InfoWindowF
-          position={{ lat: selectedLocation.lat, lng: selectedLocation.lng }}
-          onCloseClick={() => onMarkerClick(null)}
-        >
-          <div>
-            <h4 className="font-bold">{selectedLocation.name}</h4>
-            <p>{selectedLocation.address}</p>
-          </div>
-        </InfoWindowF>
-      )}
-    </GoogleMap>
+    <MapContainer center={center} zoom={4} style={{ height: '100%', width: '100%' }}>
+      <ChangeView center={selectedPosition} zoom={14} />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerClusterGroup>
+        {locations.map((location) => (
+          <Marker
+            key={location.id}
+            position={[location.lat, location.lng]}
+            icon={location.type === 'customer' ? customerIcon : supplierIcon}
+            eventHandlers={{
+              click: () => {
+                onMarkerClick(location);
+              },
+            }}
+          />
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 };
